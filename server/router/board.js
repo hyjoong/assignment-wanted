916d9f1040daa557457169e209ec167cec9c4293
hyjoong <maplesosser@naver.com>
2021-10-20 12:01:32 +0900

    feat: 특정id 값의 게시글 데이터 받아오기 구현:

--- server/router/board.js
+++ server/router/board.js
@@ -22,10 +22,25 @@ const boards = [
 const router = express.Router();
 
 // 게시글 조회
+
+// GET /boards & /boards?name=:name
 router.get("/", (req, res, next) => {
   const name = req.query.name;
   const data = name ? boards.filter((board) => board.name === name) : boards;
   res.status(200).json(data);
+});
+
+// /boards/:id
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const board = boards.filter((board) => board.id === id);
+  if (board) {
+    res.status(200).json(board);
+  } else {
+    res
+      .status(404)
+      .josn({ message: `id가 ${id}인 게시글을 찾지 못 하였습니다. ` });
+  }
 });
 
 export default router;
