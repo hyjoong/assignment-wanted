aeae76d51ca0354819617ae9c996409ea9afb9e4
hyjoong <maplesosser@naver.com>
2021-10-20 11:56:53 +0900

    feat: 게시글 전체 데이터 받아오기 & 특정 user의 게시글 데이터 받아오기 구현

--- server/router/board.js
+++ server/router/board.js
@@ -1,6 +1,31 @@
 import express from "express";
 import "express-async-errors";
 
+const boards = [
+  {
+    id: "0",
+    text: "Test ",
+    name: "hyun",
+  },
+  {
+    id: "1",
+    text: "Test2 ",
+    name: "hyun2",
+  },
+  {
+    id: "2",
+    text: "Test3 ",
+    name: "hyun3",
+  },
+];
+
 const router = express.Router();
+
+// 게시글 조회
+router.get("/", (req, res, next) => {
+  const name = req.query.name;
+  const data = name ? boards.filter((board) => board.name === name) : boards;
+  res.status(200).json(data);
+});
 
 export default router;
